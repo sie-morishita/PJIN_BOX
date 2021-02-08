714335b26e922b87e10255d903892d703b19c35a
sie_morishita <koichi.morishita@ssie.jp>
2021-02-08 12:59:47 +0900

    6回目まで

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -25,4 +25,28 @@ public class PlayerScript : MonoBehaviour
     {
         charaAnim.SetBool("isRunning", isMove);
     }
+
+    //private void OnCollisionStay(Collision collision)
+    //{
+    //    switch (collision.gameObject.tag)
+    //    {
+    //        case "Wall":
+    //            if (isMove) turn();
+    //            break;
+    //    }
+    //}
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            if (isMove) turn();
+        }
+    }
+
+    // プレイヤーの向きを反転
+    public void turn()
+    {
+        transform.Rotate(new Vector3(0, 180, 0));
+    }
 }
