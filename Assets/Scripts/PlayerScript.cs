755834e75e417d57a7029ad3dc16ff4dc8297ede
sie_morishita <koichi.morishita@ssie.jp>
2021-02-08 12:54:16 +0900

    4回目まで

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public bool isMove;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (isMove)
+        {
+            transform.position += transform.forward * 0.03f;
+        }
+    }
+}
