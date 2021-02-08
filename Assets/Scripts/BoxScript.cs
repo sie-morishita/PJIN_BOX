9ea148ca1b3a06a7a6ea9879b3793299b9b697ae
sie_morishita <koichi.morishita@ssie.jp>
2021-02-08 13:04:04 +0900

    8回目まで

--- Assets/Scripts/BoxScript.cs
+++ Assets/Scripts/BoxScript.cs
@@ -4,8 +4,48 @@ using UnityEngine;
 
 public class BoxScript : MonoBehaviour
 {
+    public enum PlayerMove { Default, MoveLeft, MoveRight }
+
+    public PlayerMove playerMove;
+
+    PlayerScript player;
+
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<PlayerScript>();
+    }
+
     public void onBoxClick()
     {
+        // プレイヤーの移動を開始する
+        switch (playerMove)
+        {
+            case PlayerMove.MoveLeft:
+                PlayerMoveLeft();
+                break;
+            case PlayerMove.MoveRight:
+                PlayerMoveRight();
+                break;
+        }
+
         Destroy(gameObject);
+    }
+
+    // 初期配置で右に移動させる場合に使う
+    void PlayerMoveRight()
+    {
+        if (!player.isMove)
+        {
+            player.MoveRight();
+        }
+    }
+
+    // 初期配置で左に移動させる場合に使う
+    void PlayerMoveLeft()
+    {
+        if (!player.isMove)
+        {
+            player.MoveLeft();
+        }
     }
 }
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -26,21 +26,13 @@ public class PlayerScript : MonoBehaviour
         charaAnim.SetBool("isRunning", isMove);
     }
 
-    //private void OnCollisionStay(Collision collision)
-    //{
-    //    switch (collision.gameObject.tag)
-    //    {
-    //        case "Wall":
-    //            if (isMove) turn();
-    //            break;
-    //    }
-    //}
-
     private void OnCollisionStay(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Wall"))
+        switch (collision.gameObject.tag)
         {
-            if (isMove) turn();
+            case "Wall":
+                if (isMove) turn();
+                break;
         }
     }
 
@@ -48,5 +40,31 @@ public class PlayerScript : MonoBehaviour
     public void turn()
     {
         transform.Rotate(new Vector3(0, 180, 0));
+    }
+
+    // プレイヤーを左に向ける
+    public void TurnLeft()
+    {
+        transform.rotation = Quaternion.Euler(0, -90, 0);
+    }
+
+    // プレイヤーを右に向ける
+    public void TurnRight()
+    {
+        transform.rotation = Quaternion.Euler(0, 90, 0);
+    }
+
+    // 左に走る
+    public void MoveLeft()
+    {
+        TurnLeft();
+        isMove = true;
+    }
+
+    // 右に走る
+    public void MoveRight()
+    {
+        TurnRight();
+        isMove = true;
     }
 }
