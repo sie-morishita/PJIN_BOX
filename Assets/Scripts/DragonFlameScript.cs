10637871abf2255aa53a32d1d9ae4b5cc370e972
sie_morishita <koichi.morishita@ssie.jp>
2021-06-29 15:05:36 +0900

    22回目まで

---
+++ Assets/Scripts/DragonFlameScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonFlameScript : MonoBehaviour
+{
+    void OnParticleCollision(GameObject other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Player":
+                other.gameObject.GetComponent<PlayerScript>().isDamaged = true;
+                break;
+        }
+    }
+}
